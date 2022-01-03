8906b22481dace83889017fce1cd7626d22f7f73
Maksym Honchar <maksim.honchar@gmail.com>
2022-01-03 18:10:23 +0200

    added bottom bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bottombar from './Bottombar';
 import Topbar from './Topbar';
 
 interface ILayout {
@@ -12,6 +13,9 @@ export function Layout(props: ILayout) {
 		<>
 			<Topbar />
 			{children}
+			<div style={{ position: 'fixed', zIndex: 1000, bottom: 0, width: "100%" }}>
+				<Bottombar />
+			</div>
 		</>
 	);
 }
