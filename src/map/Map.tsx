681d4a20a39c56c52f67416d4b93de16c580a471
Maksym Honchar <maksim.honchar@gmail.com>
2022-01-03 17:48:13 +0200

    added map object

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import {
-	MapContainer, TileLayer, Marker, Popup,
+	MapContainer, TileLayer, Marker, Polyline, Polygon,
 } from 'react-leaflet';
 
 export function Map() {
+	const position: [number, number] = [51, 0];
+
+	const polyline: [[number, number], [number, number]] = [
+		[51, 0],
+		[51.012, 0.025],
+	];
+
+	const polygon: [[number, number], [number, number], [number, number]] = [
+		[51, 0],
+		[51.0104, 0.0275],
+		[51.014, 0.023],
+	];
+
+	const limeOptions = { color: 'red' };
+
+	const polygonOptions = {
+		// dashArray: '5, 10',
+	};
+
 	return (
 		<MapContainer
-			center={[51.505, -0.09]}
+			center={position}
 			zoom={13}
 			scrollWheelZoom={false}
 			style={{ height: '100vh' }}
+			// zoomControl={false}
 		>
 			<TileLayer
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>
-					A pretty CSS3 popup.
-					{' '}
-					<br />
-					{' '}
-					Easily customizable.
-				</Popup>
-			</Marker>
+			<Marker position={position} />
+			<Polyline pathOptions={limeOptions} positions={polyline} />
+			<Polygon pathOptions={polygonOptions} positions={polygon} />
 		</MapContainer>
 	);
 }
