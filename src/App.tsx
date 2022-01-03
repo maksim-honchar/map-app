28230d4bd079b8a2345850fbf3679f03106f51d3
Maksym Honchar <maksim.honchar@gmail.com>
2022-01-03 17:31:10 +0200

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Layout } from './components/Layout';
 import { Map } from './map/Map';
 
+const theme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
 function App() {
 	return (
-		<Layout>
-			<Map />
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Map />
+			</Layout>
+		</ThemeProvider>
 	);
 }
 
