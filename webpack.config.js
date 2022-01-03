51a0fad85985f5a43e5571979e398757965bb957
Maksym Honchar <maksim.honchar@gmail.com>
2022-01-03 14:31:11 +0200

    setup react ts

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		index: './src/index.tsx',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+		hot: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './public/index.html'),
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+}
